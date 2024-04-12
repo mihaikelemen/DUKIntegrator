b4928f7d9ec84b17f5ebace91c2491f83316de81
Mihai Kelemen <mihai@webmanage.ro>
2024-04-12 17:11:38 +0200

    Lib updates to their latest available versions. Removed not needed libs

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # @Author: Mihai KELEMEN <mihai@webmanage.ro>
 
 DUKIntegratorURL=https://static.anaf.ro/static/DUKIntegrator/dist_javaInclus20200203.zip
-VERSION=1.2
+VERSION=1.3
 
 wget $DUKIntegratorURL -O DUKIntegrator.zip && unzip -qq DUKIntegrator.zip -d $PWD && rm DUKIntegrator.zip
 cp ./dist/lib/* ./lib
