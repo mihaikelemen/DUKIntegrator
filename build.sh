64bd4c3dbb4654e1b6d7704aa32df87320a3a236
Mihai Kelemen <mihai@webmanage.ro>
2024-07-27 07:31:57 +0200

    Version 1.6

--- build.sh
+++ build.sh
@@ -5,12 +5,18 @@
 # @Author: Mihai KELEMEN <mihai@webmanage.ro>
 
 DUKIntegratorURL=https://static.anaf.ro/static/DUKIntegrator/dist_javaInclus20200203.zip
-VERSION=1.5
+D112URL=https://static.anaf.ro/static/10/Anaf/Declaratii_R/AplicatiiDec/D112_16052024.zip
+D100URL=https://static.anaf.ro/static/10/Anaf/Declaratii_R/AplicatiiDec/D100_11072024.zip
+VERSION=1.6
 
 wget $DUKIntegratorURL -O DUKIntegrator.zip && unzip -qq DUKIntegrator.zip -d $PWD && rm DUKIntegrator.zip
 cp ./dist/lib/* ./lib
+mkdir -p $PWD/tmp
+wget $D112URL -O ./tmp/D112.zip && unzip -qq ./tmp/D112.zip -d ./tmp && mv ./tmp/D112*.jar
+wget $D100URL -O ./tmp/D100.zip && unzip -qq ./tmp/D100.zip -d ./tmp && mv ./tmp/D100*.jar
 docker build --no-cache -t dukintegrator:$VERSION -f ./Dockerfile .
 docker save -o ./dukintegrator-v$VERSION.tar.gz dukintegrator:$VERSION
+rm -rf ./tmp/*
 
 # cleanup
 rm -rf ./dist
